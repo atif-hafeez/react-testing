256d36ea182b36ded1757dd9663d5facc82f6611
Atif Hafeez <atif.hafeez@transcore.com>
2023-01-09 00:24:30 +0400

    FireEvent - Assertion

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
@@ -24,5 +24,21 @@ describe('App', () => {
     expect(await screen.findByText(/Signed in as/)).toBeInTheDocument();
 
     screen.debug
+  })
+})
+
+describe('App', () => {
+  it('updates the textbox', async () => {
+    render(<App />);
+
+    await screen.findByText(/Signed in as/);
+
+    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull()
+
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: {value: 'JavaScript'},
+    });
+
+    expect(screen.getByText(/Searches for JavaScript/)).toBeInTheDocument()
   })
 })
