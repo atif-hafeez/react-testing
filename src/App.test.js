57de946adc96c43391053b8098ef869b68efc837
Atif Hafeez <atif.hafeez@transcore.com>
2023-01-08 23:12:03 +0400

    SEARCH VARIANTS

--- src/App.test.js
+++ src/App.test.js
@@ -6,7 +6,23 @@ import App from './App';
 describe('App', () => {
   it('renders App component', () => {
     render(<App />);
-    
-    expect(screen.getByRole('textbox')).toBeInTheDocument();
+
+    screen.debug();
+        
+    expect(screen.queryByText(/Searches for Javascript/)).toBeNull();
   });
 });
+
+describe('App', () => {
+  it('displays Users Sign in status', async() => {
+    render(<App />);
+
+    expect(screen.queryByText(/Signed in as/)).toBeNull();
+
+    screen.debug();
+
+    expect(await screen.findByText(/Signed in as/)).toBeInTheDocument();
+
+    screen.debug
+  })
+})
